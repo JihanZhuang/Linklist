7192fc1cbd15fb83c631444582e09ce28bb99e38
JihanZhuang <zhuanghan1103@163.com>
2016-09-27 09:09:42 +0800

    linklist struct like linux

--- Linklist.c
+++ Linklist.c
@@ -8,7 +8,60 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<Linklist.h>
+typedef struct _tag_LinkList
+{
+	//带头节点的链表
+	LinkListNode header;
+	int length;
+}TLinkList;
+LinkList* LinkList_Create(){
+	TLinkList *tlink=(TLinkList *)malloc(sizeof(TLinkList));
+	if(tlink==NULL){
+		return NULL;
+	}
+	tlink->header=NULL;
+	tlink->length=0;
+	return tlink;
+}
 
+void LinkList_Destroy(LinkList* list)
+{
+	if (list == NULL)
+	{
+		return ;
+	}
+	free(list);
+	return ;
+}
+
+void LinkList_Clear(LinkList* list){
+	TLinkList *tList =NULL;
+	
+	if (list == NULL)
+	{
+		return ;
+	}
+	tList = (TLinkList *)list;
+	tList->length = 0;
+	tList->header.next = NULL;
+	return ;
+}
+
+int LinkList_Length(LinkList* list){
+
+}
+
+int LinkList_Insert(LinkList* list, LinkListNode* node, int pos){
+
+}
+
+LinkListNode* LinkList_Get(LinkList* list, int pos){
+
+}
+
+LinkListNode* LinkList_Delete(LinkList* list, int pos){
+
+}
 int main(){
 	return 1;
 }
