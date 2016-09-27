53532b9bbc06dcf88aff29d99ad19964186c27ec
JihanZhuang <zhuanghan1103@163.com>
2016-09-27 20:00:16 +0800

    complete linklist

--- Linklist.c
+++ Linklist.c
@@ -48,19 +48,77 @@ void LinkList_Clear(LinkList* list){
 }
 
 int LinkList_Length(LinkList* list){
-
+	if(list==NULL){
+		return -1;
+	}
+	return (TLinkList *)list->length;
 }
 
 int LinkList_Insert(LinkList* list, LinkListNode* node, int pos){
-
+	TLinkList *tlink=(TLinkList *)list;
+	LinkListNode *tmp=NULL;
+	int i;
+	if(tlink==NULL||node==NULL){
+		return -1;
+	}
+	tmp=&tlink->header;
+	for(i=0;i<pos&&(tmp->next!=NULL);i++){
+		tmp=tmp->next;
+	}
+	node->next=tmp->next;
+	tmp->next=node;
+	tlink->length++;
+	return tlink;
 }
 
 LinkListNode* LinkList_Get(LinkList* list, int pos){
+	int i = 0;
 
+	TLinkList *tList  = NULL;
+	LinkListNode *current = NULL;
+	LinkListNode *ret = NULL;
+	tList = (TLinkList *)list;
+
+	if (list == NULL || pos <0 ||pos>=tList->length)
+	{
+		return NULL;
+	}
+	//准备环境让辅助指针变量 指向链表头节点
+	current = &tList->header;
+	for (i=0; i<pos &&(current->next!=NULL); i++)
+	{
+		current = current->next;
+	}
+	ret = current->next;
+
+	return ret;
 }
 
 LinkListNode* LinkList_Delete(LinkList* list, int pos){
+	int i = 0;
 
+	TLinkList *tList  = NULL;
+	LinkListNode *current = NULL;
+	LinkListNode *ret = NULL;
+	tList = (TLinkList *)list;
+
+	if (list == NULL || pos <0 ||pos>=tList->length)
+	{
+		return NULL;
+	}
+	//准备环境让辅助指针变量 指向链表头节点
+	current = &tList->header;
+	for (i=0; i<pos &&(current->next!=NULL); i++)
+	{
+		current = current->next;
+	}
+	ret = current->next;
+
+	//删除算法
+	current->next =ret->next;
+	tList->length--;
+
+	return ret;
 }
 int main(){
 	return 1;
